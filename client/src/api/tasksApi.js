01cae50f0cd903871cdf0468af83d13d7a120ea9
OnlineITtuts-Tutorials <asifwebdeveloper@hotmail.com>
2025-10-20 11:42:57 +0500

    Fix CORS configuration

--- client/src/api/tasksApi.js
+++ client/src/api/tasksApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = "https://todoback-kappa.vercel.app";
 
 // Add this to debug
 console.log("API_BASE_URL:", API_BASE_URL);
